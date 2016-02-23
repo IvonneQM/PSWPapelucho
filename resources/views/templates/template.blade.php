af78052b1a621c8c70db1da5c184f86be93a8c9e
IngerGarrido <inger.mayelin@gmail.com>
2016-02-22 22:27:28 -0300

    Subida estilos casi terminados movil

--- resources/views/templates/template.blade.php
+++ resources/views/templates/template.blade.php
@@ -1,9 +1,53 @@
 @extends('templates.layout')
 @section('section')
-    <h2>   CONTENIDOOOOO</h2>
-    <p> Aqui va todo el contenido</p>
+    <h1 id="title">Bienvenidos</h1>
+    <p class="content">
+        Estimados Padres y Apoderados:
+    </p>
+    <p class="content">
+        Les damos la más cordial bienvenida a cada uno de ustedes, invitándoles a conocer el proyecto educativo de la Sala Cuna y Escuela de Párvulos “PAPELUCHO”, e instándoles a participar en la labor pedagógica que desarrollamos con mucho cariño y vocación con sus hijos(as).
+    </p>
+    <p class="content">
+        Agradecemos en forma especial la confianza de aquellos apoderados que nos han acompañado estos últimos años, como también aquellos Padres que actualmente se han integrado a esta gran familia educativa.
+    </p>
+    <p class="content">
+        Con especial afecto, les saludan.
+    </p>
+    <ul id="dir-box">
+        <li>
+            <span">Claudia Concha Gamboa</span>
+            Educadora de Párvulos
+            Directora Sede Las Colonias
+        </li>
+        <li>
+            <span>Claudia Concha Gamboa</span>
+            Educadora de Párvulos
+            Directora Sede Las Colonias
+        </li>
+    </ul>
+
 @stop
 
 @section('aside')
-    <h3> ESTE ES EL ASIDE</h3>
+    <h3 id="news-section-title">Noticias</h3>
+    <ul id="news-box">
+        <li >
+    <p class="news-first-line">
+        <span class="news-title">Titulo 1</span>
+        <span class="news-date">02-03-16</span>
+    </p>
+    <p class="news-second-line">
+        Aqui va el contenido de las noticias y bla bla bla
+    </p>
+        </li>
+        <li >
+            <p class="news-first-line">
+                <span class="news-title">Titulo 2</span>
+                <span class="news-date">05-04-16</span>
+            </p>
+            <p class="news-second-line">
+                Aqui va el contenido de las noticias y bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla
+            </p>
+        </li>
+    </ul>
 @stop
