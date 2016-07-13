f5218933a3c1f4ea3f6f6366284835437a950ce1
IvonneQM <ivonneqm@gmail.com>
2016-07-12 23:08:58 -0400

    fields 100%

--- resources/views/cms/admin/archivos/partials/form.blade.php
+++ resources/views/cms/admin/archivos/partials/form.blade.php
@@ -22,34 +22,28 @@
                 {!! Field::select('galeria_id',$galerias,['name' => 'galerias' ]) !!}
                 {!! Form::close()!!}
                 {{--button type="submit" class="btn-buscar"><i class="fa fa-search"></i></button>--}}
-                <div class="col-md-5 div-btn">
-                    <a class="btn btn-primary pull-right btn-crear-nuevo"
-                       href="{{ route('administrador.galerias.index') }}">
-                        <i class="fa fa-picture-o"> <span class="button-title">Crear Galería</span></i></a>
-                </div>
+
+            </div>
+            <div class="col-lg-4 btn-galery">
+                <a class="btn btn-primary pull-right btn-crear-nuevo"
+                   href="{{ route('administrador.galerias.index') }}">
+                    <i class="fa fa-picture-o"> <span class="button-title">Crear Galería</span></i></a>
             </div>
         </div>
         <div role="tabpanel" class="tab-pane panel-option" id="informe">
             <div class="col-lg-8 container-option">
                 {!! Form::open(['class' => 'form']) !!}
                 {{-- {!! Form::label('nombreParvulo','Ingrese nombre del párvulo: ') !!} --}}
-                {!! Field::text('Párvulo',['class'=>'easy-autocomplete','id'=>'usera']) !!}
+                {!! Field::text('Párvulo',['class'=>'easy-autocomplete','id'=>'user', 'placeholder'=>'Ingrese un párvulo']) !!}
                 {!! Form::hidden('parvulos',null, ['id'=>'user_id']) !!}
                 {!! Form::close()!!}
             </div>
-
-
         </div>
 
         <div role="tabpanel" class="tab-pane panel-option" id="boletin">
             <div class="col-lg-8 container-option">
                 {!! Form::open(['class' => 'form']) !!}
-                <label for="sel4">Nivel: </label>
-                <select name="niveles" id="sel4" >
-                    @foreach($niveles as $nivel)
-                        <option value="{{$nivel->getKey()}}">{{$nivel->name}}</option>
-                    @endforeach
-                </select>
+                {!! Field::select('Niveles',\App\Nivel::lists('name','id')->toArray(),['name'=>'niveles', 'class'=>'form']) !!}
                 {!! Form::close()!!}
             </div>
         </div>
@@ -57,46 +51,3 @@
     </div>
     {!! Form::hidden('type', 'galerias-jardines' , array('id' => 'type')) !!}
 </div>
-
-<script>
-    $(document).ready(function () {
-        $("#user").easyAutocomplete({
-            url: "autocomplete/parvulos",
-            getValue: "full_name",
-            template: {
-                type  : "description",
-                fields: {
-                    description: "rut"
-                }
-            },
-            list: {
-                match: {
-                    enabled: true
-                },
-
-                onSelectItemEvent: function() {
-                    var user = $("#user").getSelectedItemData();
-                    $("#user_id").val(user.id);
-                }
-            },
-            theme: "bootstrap",
-
-            ajaxSettings: {
-                dataType: "json",
-                method: "GET",
-                data: {
-                }
-            },
-
-            preparePostData: function(data) {
-                data.search = $("#user").val();
-                return data;
-            },
-
-            requestDelay: 500
-
-        }).change(function () {
-            $('#user_id').val('')
-        })
-    });
-</script>
